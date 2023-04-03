ac434f7a496d3f8793198719a18b9039ded0996b
huyeduon <huyeduon@cisco.com>
2023-04-03 18:46:14 +0800

    block s3 public access

---
+++ blocks3.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import boto3
+from botocore.config import Config
+
+htduong01 = Config(
+    region_name='us-east-1',
+    retries={
+        'max_attempts': 10,
+        'mode': 'standard'
+    }
+)
+
+session = boto3.session.Session(profile_name='htduong01')
+s3 = session.client('s3', config=htduong01)
+
+# Output the bucket names
+def listBuckets():
+    response = s3.list_buckets()
+    for bucket in response['Buckets']:
+        print(f'  {bucket["Name"]}')
+
+def blockAccess():
+    allbuckets = s3.list_buckets()
+    # Iterate over the list
+    for bucket in allbuckets['Buckets']:
+        try:
+            # This will set the public block settings
+            s3.put_public_access_block(
+                Bucket=bucket['Name'],
+                PublicAccessBlockConfiguration={
+                    'BlockPublicAcls': True,
+                    'IgnorePublicAcls': True,
+                    'BlockPublicPolicy': True,
+                    'RestrictPublicBuckets': True
+                }
+            )
+        except:
+            pass
+def main():
+
+    print('Existing buckets:')
+    listBuckets()
+    print('Block all public access')
+    blockAccess()
+
+if __name__ == main():
+    main()
